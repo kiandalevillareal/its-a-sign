871b354a0e59da9fdb0a2dd394482f2c18cd79b9
Jerald E. Casulla <dlarejcasulla12@gmail.com>
2023-06-22 00:03:32 +0800

    41

--- www/backend/API/endSession.php
+++ www/backend/API/endSession.php
@@ -10,20 +10,20 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, oAccess-Control-Allow-Headers, Authorization, X-Requested-With");
 include_once '../database.php';
-include_once '../user.php';
+include_once '../session.php';
 
 $database = new Database();
 $db = $database->getConnection();
-$item = new User($db);
+$item = new Session($db);
 // $item->username = isset($_POST['username']) ? $_POST['username'] : die();
-$item->username = $_POST['username'];
+$item->session_id = $_POST['session_id'];
 
-if($item->checkUser())
+if($item->endSession())
 {
-    echo json_encode("username has taken");
+    echo json_encode("session ended");
 }
 else
 {
-    echo json_encode("username is available");
+    echo json_encode("session not ended");
 }
 ?>
