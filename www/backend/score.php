91f3a8ed427c605c2ab79f431586a500543a3bfb
Jerald E. Casulla <dlarejcasulla12@gmail.com>
2023-06-28 03:22:19 +0800

    leaderboard

--- www/backend/score.php
+++ www/backend/score.php
@@ -9,7 +9,9 @@ class Score
     public $user_id;
     public $difficulty;
     public $score;
-    public $word;
+    public $sentence;
+    public $score_id;
+    
 
     public function __construct($db)
     {
@@ -21,18 +23,124 @@ class Score
         $this->user_id = htmlspecialchars(strip_tags($this->user_id));
         $this->difficulty = htmlspecialchars(strip_tags($this->difficulty));
         $this->score = htmlspecialchars(strip_tags($this->score));
-        $this->word = htmlspecialchars(strip_tags($this->word));
+        $this->sentence = htmlspecialchars(strip_tags($this->sentence));
 
         $query = "INSERT INTO scores SET 
         user_id = ".$this->user_id.",
         difficulty = '".$this->difficulty."',
         score = ".$this->score.",
-        word = '".$this->word."'";
+        sentence = '".$this->sentence."'";
 
         $this->db->query($query);
 
         if($this->db->affected_rows > 0) return true;
         else return false;
     }
+    public function getLeaderboard()
+    {
+        $this->difficulty = htmlspecialchars(strip_tags($this->difficulty));
+        $this->score_id = htmlspecialchars(strip_tags($this->score_id));
+        
+// SELECT s.user_id, s.score_id, s.score, s.sentence
+// FROM scores AS s
+// JOIN users AS u ON s.user_id = u.user_id
+// JOIN (
+//   SELECT user_id, MAX(score) AS max_score
+//   FROM scores
+//   WHERE difficulty = 'easy'
+//   GROUP BY user_id
+// ) AS max_scores ON s.user_id = max_scores.user_id AND s.score = max_scores.max_score
+// WHERE s.difficulty = 'easy'
+// GROUP BY s.user_id
+// ORDER BY s.score DESC
+
+// SELECT s.user_id, s.score_id, s.score, s.sentence
+// FROM scores AS s
+// JOIN users AS u ON s.user_id = u.user_id
+// JOIN (
+//   SELECT user_id, MAX(score) AS max_score
+//   FROM scores
+//   WHERE difficulty = 'easy'
+//   GROUP BY user_id
+// ) AS max_scores ON s.user_id = max_scores.user_id AND s.score = max_scores.max_score
+// WHERE s.difficulty = 'easy'
+//   AND s.score < 300
+//   AND s.user_id = 8
+// GROUP BY s.user_id
+// ORDER BY s.score DESC
+
+        $topScoresQuery = 
+            "SELECT  ROW_NUMBER() OVER (ORDER BY s.score DESC) AS position,
+            s.user_id, username, s.score_id, s.score, s.sentence
+            FROM scores AS s
+            JOIN users AS u ON s.user_id = u.user_id
+            JOIN (
+              SELECT user_id, MAX(score) AS max_score
+              FROM scores
+              WHERE difficulty = '".$this->difficulty."'
+              GROUP BY user_id
+            ) AS max_scores ON s.user_id = max_scores.user_id AND s.score = max_scores.max_score
+            WHERE s.difficulty = '".$this->difficulty."'
+            GROUP BY s.user_id
+            ORDER BY s.score DESC";
+
+        $UserScoreQuery = 
+            "SELECT  ROW_NUMBER() OVER (ORDER BY s.score DESC) AS position,
+            s.user_id, username, s.score_id, s.score, s.sentence
+            FROM scores AS s
+            JOIN users AS u ON s.user_id = u.user_id
+            JOIN (
+              SELECT user_id, MAX(score) AS max_score
+              FROM scores
+              WHERE difficulty = '".$this->difficulty."'
+              GROUP BY user_id
+            ) AS max_scores ON s.user_id = max_scores.user_id AND s.score = max_scores.max_score
+            WHERE s.difficulty = '".$this->difficulty."'
+            AND s.user_id = '".$this->user_id."'
+            GROUP BY s.user_id
+            ORDER BY s.score DESC";
+
+        $result = $this->db->query($topScoresQuery);
+        $result2 = $this->db->query($UserScoreQuery);
+        // $userPosition = ($result2->num_rows > 0) ? ($result2->fetch_assoc())['position'] + 1 : null;
+
+
+        if ($result->num_rows > 0) {
+            // Process and return the top scores as JSON
+            $topScores = [];
+            $position = 1; // Initialize position counter
+    
+            for ($position = 1; $position <= 3 && $row = $result->fetch_assoc(); $position++) {
+                // Create a new row with position and other data
+                $newRow = [
+                    'position' => $row['position'],
+                    'user_id' => $row['user_id'],
+                    'username' => $row['username'],
+                    'score_id' => $row['score_id'],
+                    'score' => $row['score'],
+                    'sentence' => $row['sentence']
+                ];
+            
+                $topScores[] = $newRow;
+            }
+           while ($row = $result2->fetch_assoc()) {
+                // Create a new row with position and other data
+                $newRow = [
+                    'position' => $row['position'],
+                    'user_id' => $row['user_id'],
+                    'username' => $row['username'],
+                    'score_id' => $row['score_id'],
+                    'score' => $row['score'],
+                    'sentence' => $row['sentence']
+                ];
+        
+                $topScores[] = $newRow;
+            }
+            return $topScores;
+        } else {
+            return "Leaderboard not retrieved.";
+        }
+    }
+    
 }
 ?>
--- www/js/controller/leaderbordsCon.js
+++ www/js/controller/leaderbordsCon.js
@@ -1,25 +1,73 @@
 const gameoverButton = document.getElementById('gameover-button');
+const sendScoreButton = document.getElementById('send-highscore-button');
+const refreshScoreButton = document.getElementById('refresh-leaderboard-button');
+
 const gameoverContainer = document.getElementById('gameover-container-bg');
 const highscoreForm = document.getElementById("send-highscore-form");
 
+const usernameText = document.getElementById("username-text");
+const difficultyText = document.getElementById("difficulty-text");
+const scoreText = document.getElementById("score-text");
+const sentenceText = document.getElementById("sentence-text");
+
+const loadingAnimationLeaderBoardRefresh =
+    document.getElementById("loading-animation-leaderboard-refresh");
+const loadingAnimationLeaderBoardSend =
+    document.getElementById("loading-animation-leaderboard-send");
+
+
 let isGameOverOn = false;
+
+// Test Case
+let user_id_test = 5;
+let difficulty_test = 'hard';
+let score_test = 3.23;
+let sentence_test = "ano ikaw tao ba";
 
 // Add event listener to the form submission
 highscoreForm.addEventListener("submit", submitHighscore);
 
+refreshScoreButton.addEventListener('click', function ()
+{
+    // Code to be executed when the button is clicked
+    console.log('Button refresh clicked!');
+});
+
 gameoverButton.addEventListener('click', () =>
 {
     isGameOverOn = !isGameOverOn;
-    if (isGameOverOn) gameoverContainer.style.display = "flex";
-    else gameoverContainer.style.display = "none";
+    if (isGameOverOn)
+    {
+        gameoverContainer.style.display = "flex";
+        gameIsOver();
+    } else
+    {
+        gameoverContainer.style.display = "none";
+    }
 });
+
+function gameIsOver()
+{
+    usernameText.textContent = user_id_test;
+    difficultyText.textContent = difficulty_test;
+    scoreText.textContent = score_test;
+    sentenceText.textContent = sentence_test;
+}
 
 
 function submitHighscore(event)
 {
     event.preventDefault(); // Prevent default form  submission
 
+    sendScoreButton.style.display = "none";
+    loadingAnimationLeaderBoardSend.style.display = "inline-block";
+
     const formData = new FormData(highscoreForm);
+
+    formData.append('user_id', user_id_test);
+    formData.append('difficulty', difficulty_test);
+    formData.append('score', score_test);
+    formData.append('word', sentence_test);
 
     consoleLogger("sending");
 
@@ -52,10 +100,7 @@ function submitHighscore(event)
         })
         .finally(() =>
         {
+            sendScoreButton.style.display = "inline-block";
+            loadingAnimationLeaderBoardSend.style.display = "none";
         });
-
-    // Reset the form after submission
-    // loginForm.reset();
-    // usernameInput.focus();
-
 }
